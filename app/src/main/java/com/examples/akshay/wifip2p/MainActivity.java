0a162a8e550e7380dce81d4fa036051b8b930245
akshay-ap <akshay.ap95@gmail.com>
2018-02-14 23:42:43 +0530

    Implements primitive string transfer

--- app/src/main/java/com/examples/akshay/wifip2p/MainActivity.java
+++ app/src/main/java/com/examples/akshay/wifip2p/MainActivity.java
@@ -1,14 +1,22 @@
 package com.examples.akshay.wifip2p;
 
-import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "===MainActivity";
@@ -16,9 +24,16 @@ public class MainActivity extends AppCompatActivity {
     WifiP2pManager.Channel mChannel;
     WifiBroadcastReceiver mReceiver;
     IntentFilter mIntentFilter;
+    public static WifiP2pDevice device = null;
+    public static final String DEVICE_TO_SEARCH = "moto m";
 
 
     Button buttonDiscover = null;
+    Button buttonConnect = null;
+    Button buttonSend = null;
+    Button buttonReceive = null;
+    TextView textViewGroupOwner = null;
+    ListView listViewDevices = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
 
         setUpIntentFilter();
         setUpUI();
-
         mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         mChannel = mManager.initialize(this, getMainLooper(), null);
         mReceiver = new WifiBroadcastReceiver(mManager, mChannel, this);
@@ -58,6 +72,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpUI() {
         buttonDiscover = findViewById(R.id.main_activity_button_discover);
+        buttonConnect = findViewById(R.id.main_activity_button_connect);
+        buttonReceive = findViewById(R.id.main_activity_button_receive);
+        buttonSend = findViewById(R.id.main_activity_button_send);
+        textViewGroupOwner = findViewById(R.id.main_activity_textview_owner);
+        listViewDevices = findViewById(R.id.main_activity_list_view_devices);
 
         buttonDiscover.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -65,6 +84,31 @@ public class MainActivity extends AppCompatActivity {
                 discoverPeers();
             }
         });
+
+        buttonConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                connect();
+            }
+        });
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            SendDataTask sendDataTask = new SendDataTask(getApplicationContext());
+            sendDataTask.execute();
+            }
+        });
+
+        buttonReceive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReceiveDataTask receiveDataTask = new ReceiveDataTask(getApplicationContext());
+                receiveDataTask.execute();
+            }
+        });
+
+
     }
 
     private void discoverPeers()
@@ -82,10 +126,49 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(int i) {
-                Log.d(MainActivity.TAG," peer discovery failed");
-
+                if (i == WifiP2pManager.P2P_UNSUPPORTED) {
+                    Log.d(MainActivity.TAG," peer discovery failed :" + "P2P_UNSUPPORTED");
+                } else if (i == WifiP2pManager.ERROR) {
+                    Log.d(MainActivity.TAG," peer discovery failed :" + "ERROR");
+                } else if (i == WifiP2pManager.BUSY) {
+                    Log.d(MainActivity.TAG," peer discovery failed :" + "BUSY");
+                }
             }
         });
 
     }
+
+    public void connect () {
+        // Picking the first device found on the network.
+
+        WifiP2pConfig config = new WifiP2pConfig();
+        config.deviceAddress = device.deviceAddress;
+        config.wps.setup = WpsInfo.PBC;
+
+        mManager.connect(mChannel, config, new WifiP2pManager.ActionListener() {
+
+            @Override
+            public void onSuccess() {
+                Log.d(MainActivity.TAG, "Connected to :" + DEVICE_TO_SEARCH);
+                Toast.makeText(getApplication(),"Connection successful with " + DEVICE_TO_SEARCH,Toast.LENGTH_SHORT).show();
+
+              /*  mManager.requestGroupInfo(mChannel, new WifiP2pManager.GroupInfoListener() {
+                    @Override
+                    public void onGroupInfoAvailable(WifiP2pGroup wifiP2pGroup) {
+                        WifiP2pDevice wifiP2pDevice = wifiP2pGroup.getOwner();
+                        Log.d(MainActivity.TAG,"onGroupInfoAvailable " + wifiP2pDevice.deviceName);
+                        textViewGroupOwner.setText(wifiP2pDevice.deviceName);
+                    }
+                });*/
+            }
+
+            @Override
+            public void onFailure(int reason) {
+                Log.d(MainActivity.TAG, "Connect failed. Retry.");
+            }
+        });
+    }
+
+
+
 }
--- app/src/main/java/com/examples/akshay/wifip2p/MyPeerListener.java
+++ app/src/main/java/com/examples/akshay/wifip2p/MyPeerListener.java
@@ -5,24 +5,39 @@ import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by ash on 14/2/18.
  */
 
 public class MyPeerListener implements WifiP2pManager.PeerListListener {
     public static final String TAG = "===MyPeerListener";
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+
     @Override
     public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
-        Log.d(MyPeerListener.TAG,"OnPeerAvailable()");
+
+        Log.d(MyPeerListener.TAG, "OnPeerAvailable()");
         if(wifiP2pDeviceList != null ) {
+
+            if(wifiP2pDeviceList.getDeviceList().size() == 0) {
+                Log.d(MyPeerListener.TAG, "wifiP2pDeviceList size is zero");
+                return;
+            }
+            Log.d(MainActivity.TAG,"");
             for (WifiP2pDevice device : wifiP2pDeviceList.getDeviceList()) {
-                Log.d(MyPeerListener.TAG,"Device name :"+device.deviceName);
+                if(device.deviceName.equals(MainActivity.DEVICE_TO_SEARCH)) {
+                    MainActivity.device = device;
+                } else {
+                    Log.d(MyPeerListener.TAG, "Found device :" + device.deviceName + " " + device.deviceAddress);
+                }
             }
         }
         else {
-            Log.d(MyPeerListener.TAG," wifiP2pDeviceList is null");
+            Log.d(MyPeerListener.TAG, "wifiP2pDeviceList is null");
 
         }
     }
-
 }
